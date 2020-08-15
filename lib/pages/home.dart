cab05bfeb963df3ee0942ff02068b4f529a0c879
Aravind Siruvuru <siruvuruaravind@gmail.com>
2020-08-15 15:05:44 +0530

    un auth scaffold designed for google signin

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,8 +13,47 @@ class _HomeState extends State<Home> {
     return Text('auth screen');
   }
 
-  Widget buildUnAuthScreen() {
-    return Text('auth screen');
+  Scaffold buildUnAuthScreen() {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [Colors.teal, Colors.purple],
+          ),
+        ),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Flluttershare',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white70,
+                fontFamily: "Signatra",
+                fontSize: 90.0,
+              ),
+            ),
+            GestureDetector(
+              onTap: () => print('tapped google'),
+              child: Container(
+                width: 260.0,
+                height: 60.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image:
+                          AssetImage('assets/images/google_signin_button.png'),
+                      fit: BoxFit.cover),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   @override
